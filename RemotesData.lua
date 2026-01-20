d1499ce72241009c26955a752e4b404e86b17604
gotham10 <notagainbruh1236@outlook.com>
2026-01-20 03:18:30 -0600

    Create RemotesData.lua

---
+++ RemotesData.lua
@@ -0,0 +1,19 @@
+return {
+    MotHitH1 = "IucpoZdgwp",
+    MotHitH2 = "\a\240\159\164\163\240\159\164\161\a\n\a\n\a\nefmmgivC",
+    CropPlaceH1 = "dZnpyRtxna",
+    CropPlaceH2 = "\a\240\159\164\163\240\159\164\161\a\n\a\n\a\nsDahbvdxZludavlcoipDDMYasPlcm",
+    PickupH1 = "oesmuqhxddgxp",
+    PickupH2 = "\a\240\159\164\163\240\159\164\161\a\n\a\n\a\ngnerJcfMftkYelk",
+    PlaceHASHName = "uwhiHAMdjExWka",
+    PlaceHASH = "\a\240\159\164\163\240\159\164\161\a\n\a\n\a\nffEgdldU",
+    BreakHASHName = "Xoeoxuqilfgenamojfjmj",
+    BreakHASH = "\a\240\159\164\163\240\159\164\161\a\n\a\n\a\nohIstskUiftvgjy",
+    shootSpellName = "zkPhNEfenvrvyfjtngsdjEZ",
+    shootSpellHashName = "\7\240\159\164\163\240\159\164\161\7\n\7\n\7\ngkbrvenfosed",
+    KillRemote = "fLafXsVXagmlXhlc/UlpaomJfNzwc",
+    AngelRemote = "bybFwzowayukxnCeCx/ecxnnzkznjuFaqtmlrjrov",
+    FishFarmFinishRemote = "bybFwzowayukxnCeCx/iNxxqhXmhuflLjt",
+    shootSpellBookRemote = "fLafXsVXagmlXhlc/wukvoquydkgMcywiyDwvjc",
+    SpiritRemote = ""
+}
