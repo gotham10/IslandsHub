4302dd909d240939739d40cdf9ce574b31dce208
gotham10 <notagainbruh1236@outlook.com>
2026-02-06 19:38:40 -0600

    Add RemotesDataPrinter.lua with hash data

---
+++ RemotesDataPrinter.lua
@@ -0,0 +1,6 @@
+return {
+    PlaceHASHName = "uwhiHAMdjExWka",
+    PlaceHASH = "\7\240\159\164\163\240\159\164\161\7\n\7\n\7\nffEgdldU",
+    BreakHASHName = "Xoeoxuqilfgenamojfjmj",
+    BreakHASH = "\7\240\159\164\163\240\159\164\161\7\n\7\n\7\nohIstskUiftvgjy",
+}
